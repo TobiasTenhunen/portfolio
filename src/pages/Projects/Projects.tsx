5b0abf184a418ec23f1d38cd359870d1c0fc4b68
TobiasTenhunen <tobiastenhunen.mail@gmail.com>
2026-06-11 21:47:59 +0200

    Improved projects page. Added a footer to the layout but doesn't quite work yet.

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import "./Projects.css"
 
 // Project images
 import bakkieBewust from "../../assets/project-showcases/bakkie-bewust.webp"
+import bauhaus from "../../assets/project-showcases/bauhaus.webp"
 
 export default function Projects() {
     return (
@@ -14,6 +15,11 @@ export default function Projects() {
                     projectName="Bakkie Bewust"
                     imageUrl={bakkieBewust}
                     description="Een fictief bedrijf voor duurzame koffie."
+                />
+                <ProjectTile
+                    projectName="Bauhaus Parallax Effect"
+                    imageUrl={bauhaus}
+                    description="Een school opdracht waarbij we een site moesten maken met een parallax effect."
                 />
             </section>
         </>
