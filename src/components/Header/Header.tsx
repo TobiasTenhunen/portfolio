bd6a2e97216c3c60aa0f47db17045744ef8b982d
TobiasTenhunen <tobiastenhunen.mail@gmail.com>
2026-09-23 18:18:16 +0200

    Update the logo to a newer version. New white favicon

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,8 +19,8 @@ export default function Header() {
         <header>
             <Link className="header-logo" to="/">
                 <div>
-                    <svg id="Laag_1" data-name="Laag 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180.99 126.2">
-                        <path ref={pathRef} d="M180.99 0v8.6l-38.6 4v105.6l-13.4 8V12.6H52v113.6l-13.4-8V12.6L0 8.6V0z"></path>
+                    <svg id="Laag_1" data-name="Laag 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 183.82 130.11">
+                        <path ref={pathRef} d="M128.99 15.44L54.83 15.44L54.83 130.11L38.6 120.42L38.6 15.3L0 11.3L0 0L183.82 0L183.82 11.3L145.22 15.3L145.22 120.42L128.99 130.11L128.99 15.44z"></path>
                     </svg>
                 </div>
             </Link>
